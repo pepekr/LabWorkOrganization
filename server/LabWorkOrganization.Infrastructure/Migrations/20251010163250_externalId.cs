a4b80d912fa7d88032e496037f6b6a8a51184de5
illia <illiakrang@gmail.com>
2025-10-10 19:34:39 +0300

    feat: add external id for services like google class

---
+++ server/LabWorkOrganization.Infrastructure/Migrations/20251010163250_externalId.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LabWorkOrganization.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class externalId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ExternalId",
+                table: "Tasks",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ExternalId",
+                table: "Courses",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExternalId",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "ExternalId",
+                table: "Courses");
+        }
+    }
+}
