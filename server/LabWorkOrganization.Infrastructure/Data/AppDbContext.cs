1e4daf9494386df6a3541a85bfbde5d951119581
illia <illiakrang@gmail.com>
2025-10-10 16:01:33 +0300

    feat: add db context

--- server/LabWorkOrganization.Infrastructure/Data/AppDbContext.cs
+++ server/LabWorkOrganization.Infrastructure/Data/AppDbContext.cs
@@ -1,8 +1,27 @@
-﻿using Microsoft.EntityFrameworkCore;
+using LabWorkOrganization.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace LabWorkOrganization.Infrastructure.Data
 {
-    public class AppDbContext(DbContextOptions<AppDbContext> options):DbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+           : base(options)
+        {
+        }
+        public DbSet<Course> Course { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<UserTask> UserTasks { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<SubGroup> SubGroups { get; set; }
+        public DbSet<QueuePlace> QueuePlaces { get; set; }
+        public DbSet<LabTask> Tasks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
     }
 }
---
+++ server/LabWorkOrganization.Infrastructure/Migrations/20251010122402_InitialCreate.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LabWorkOrganization.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Course",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    LessonDuration = table.Column<TimeSpan>(type: "interval", nullable: false),
+                    EndOfCourse = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Course", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SubGroups",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    AllowedDays = table.Column<int[]>(type: "integer[]", nullable: false),
+                    CourseId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SubGroups", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SubGroups_Course_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Course",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    DueDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    IsSentRequired = table.Column<bool>(type: "boolean", nullable: false),
+                    TimeLimitPerStudent = table.Column<TimeSpan>(type: "interval", nullable: false),
+                    CourseId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tasks_Course_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Course",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    PasswordHash = table.Column<string>(type: "text", nullable: false),
+                    RoleId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CourseId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Users_Course_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Course",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Users_Roles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "QueuePlaces",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    SubGroupId = table.Column<Guid>(type: "uuid", nullable: false),
+                    SpecifiedTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QueuePlaces", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_QueuePlaces_SubGroups_SubGroupId",
+                        column: x => x.SubGroupId,
+                        principalTable: "SubGroups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_QueuePlaces_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SubGroupUser",
+                columns: table => new
+                {
+                    StudentsId = table.Column<Guid>(type: "uuid", nullable: false),
+                    SubGroupsId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SubGroupUser", x => new { x.StudentsId, x.SubGroupsId });
+                    table.ForeignKey(
+                        name: "FK_SubGroupUser_SubGroups_SubGroupsId",
+                        column: x => x.SubGroupsId,
+                        principalTable: "SubGroups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SubGroupUser_Users_StudentsId",
+                        column: x => x.StudentsId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserTasks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    TaskId = table.Column<Guid>(type: "uuid", nullable: false),
+                    IsCompleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserTasks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserTasks_Tasks_TaskId",
+                        column: x => x.TaskId,
+                        principalTable: "Tasks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserTasks_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QueuePlaces_SubGroupId",
+                table: "QueuePlaces",
+                column: "SubGroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QueuePlaces_UserId",
+                table: "QueuePlaces",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SubGroups_CourseId",
+                table: "SubGroups",
+                column: "CourseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SubGroupUser_SubGroupsId",
+                table: "SubGroupUser",
+                column: "SubGroupsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_CourseId",
+                table: "Tasks",
+                column: "CourseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_CourseId",
+                table: "Users",
+                column: "CourseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_RoleId",
+                table: "Users",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserTasks_TaskId",
+                table: "UserTasks",
+                column: "TaskId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserTasks_UserId",
+                table: "UserTasks",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "QueuePlaces");
+
+            migrationBuilder.DropTable(
+                name: "SubGroupUser");
+
+            migrationBuilder.DropTable(
+                name: "UserTasks");
+
+            migrationBuilder.DropTable(
+                name: "SubGroups");
+
+            migrationBuilder.DropTable(
+                name: "Tasks");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Course");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+        }
+    }
+}
