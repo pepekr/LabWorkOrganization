c55368bdca14e302c15fe64865ba32d217ef3402
illia <illiakrang@gmail.com>
2025-10-10 13:29:38 +0300

    feat: add configuration for db connection string

--- server/LabWorkOrganization.Infrastructure/DependencyInjection.cs
+++ server/LabWorkOrganization.Infrastructure/DependencyInjection.cs
@@ -1,16 +1,17 @@
 ﻿using LabWorkOrganization.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace LabWorkOrganization.Infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructureDI(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructureDI(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddDbContext<AppDbContext>(options =>
             {
-                options.UseNpgsql("DefaultConnection");
+                options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
             });
             return services;
         }
