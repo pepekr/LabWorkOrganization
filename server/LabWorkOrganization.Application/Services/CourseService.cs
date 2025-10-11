de08ea7f8abe4e57117d0e7678b2e9825b8a3ea9
illia <illiakrang@gmail.com>
2025-10-11 18:39:27 +0300

    feat: add external logic to course service, change IExternalRepo to be fully async

--- server/LabWorkOrganization.Application/Services/CourseService.cs
+++ server/LabWorkOrganization.Application/Services/CourseService.cs
@@ -6,39 +6,90 @@ namespace LabWorkOrganization.Application.Services
     public class CourseService
     {
         private readonly ICrudRepository<Course> _crudRepository;
+        private readonly IExternalCrudRepo<Course> _externalCrudRepository;
         private readonly IUnitOfWork _unitOfWork;
-        public CourseService(ICrudRepository<Course> crudRepository, IUnitOfWork IUnitOfWork)
+        public CourseService(ICrudRepository<Course> crudRepository, IUnitOfWork IUnitOfWork, IExternalCrudRepo<Course> IExternalCrudRepository)
         {
             _crudRepository = crudRepository;
             _unitOfWork = IUnitOfWork;
+            _externalCrudRepository = IExternalCrudRepository;
         }
-        public async Task<Course> CreateCourse(Course course)
+
+        // TODO: ALL METHODS HERE MUST RETUR RESULT TYPE WITH SUCCESS FLAG AND ERROR MESSAGE IF NEEDED
+        public async Task<Course> CreateCourse(Course course, bool useExternal)
         {
-            await _crudRepository.AddAsync(course);
-            await _unitOfWork.SaveChangesAsync();
-            return course;
+            try
+            {
+                if (useExternal)
+                {
+                    await _externalCrudRepository.AddAsync(course);
+                }
+                await _crudRepository.AddAsync(course);
+                await _unitOfWork.SaveChangesAsync();
+                return course;
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"An error occurred while creating the course: {ex.Message}");
+                throw;
+            }
         }
-        public async Task<Course?> GetCourseById(Guid id)
+        public async Task<Course?> GetCourseById(Guid id, bool external)
         {
-            return await _crudRepository.GetByIdAsync(id);
+            try
+            {
+                if (external)
+                {
+                    return await _externalCrudRepository.GetByIdAsync(id);
+                }
+                return await _crudRepository.GetByIdAsync(id);
+            }
+            catch (Exception ex) { throw; }
         }
-        public async Task<IEnumerable<Course>> GetAllCourses()
+        public async Task<IEnumerable<Course>> GetAllCourses(bool isGetExternal)
         {
-            return await _crudRepository.GetAllAsync();
+            try
+            {
+                var courses = await _crudRepository.GetAllAsync();
+                IEnumerable<Course> togetherCourses = courses ?? Enumerable.Empty<Course>();
+                if (isGetExternal)
+                {
+                    togetherCourses = (courses ?? Enumerable.Empty<Course>())
+                       .Concat(await _externalCrudRepository.GetAllAsync() ?? Enumerable.Empty<Course>());
+                }
+                return togetherCourses;
+            }
+            catch (Exception ex) { throw; }
         }
-        public async void UpdateCourse(Course course)
+        public async void UpdateCourse(Course course, bool updateExternal)
         {
-            _crudRepository.Update(course);
-            await _unitOfWork.SaveChangesAsync();
+            try
+            {
+                if (updateExternal)
+                {
+                    await _externalCrudRepository.UpdateAsync(course);
+                }
+                _crudRepository.Update(course);
+                await _unitOfWork.SaveChangesAsync();
+            }
+            catch (Exception ex) { throw; }
         }
 
         public async void DeleteCourse(Guid id)
         {
-            var course = await _crudRepository.GetByIdAsync(id);
-            if (course != null)
+            try
             {
-                _crudRepository.Delete(course);
-                await _unitOfWork.SaveChangesAsync();
+                var course = await _crudRepository.GetByIdAsync(id);
+                if (course != null)
+                {
+                    _crudRepository.Delete(course);
+                    await _unitOfWork.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
             }
         }
     }
--- server/LabWorkOrganization.Domain/Intefaces/IExternalCrudRepo.cs
+++ server/LabWorkOrganization.Domain/Intefaces/IExternalCrudRepo.cs
@@ -6,7 +6,7 @@ namespace LabWorkOrganization.Domain.Intefaces
         public Task<TEntity> AddAsync(TEntity entity);
         public Task<TEntity?> GetByIdAsync(Guid id);
         public Task<IEnumerable<TEntity>> GetAllAsync();
-        public void Update(TEntity entity);
-        public void Delete(TEntity entity);
+        public Task<TEntity> UpdateAsync(TEntity entity);
+        public Task<TEntity> DeleteAsync(TEntity entity);
     }
 }
