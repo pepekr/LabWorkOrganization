503480b0a03efa7be6d05fbcf502ea891d219822
illia <illiakrang@gmail.com>
2025-10-12 22:51:04 +0300

    feat: unfinished client needs mapping

--- server/LabWorkOrganization.Infrastructure/Data/ExternalAPIs/Clients/ClassroomClient.cs
+++ server/LabWorkOrganization.Infrastructure/Data/ExternalAPIs/Clients/ClassroomClient.cs
@@ -1,13 +1,69 @@
 using LabWorkOrganization.Domain.Intefaces;
+using System.Net.Http.Json;
+using System.Text.Json;
 
 namespace LabWorkOrganization.Infrastructure.Data.ExternalAPIs.Clients
 {
-    public class ClassroomClient<TEntity> : IExternalCrudRepo<TEntity>
+    public class ClassroomClient<TEntity, TResponse> : IExternalCrudRepo<TEntity>
+        where TEntity : class
     {
-        // need to implement call to the external API (classroom) and map to entity objects
-        public Task<IEnumerable<TEntity>> GetAllAsync()
+        private readonly string _accessToken;
+        private readonly HttpClient _httpClient;
+        private readonly string _baseUrl;
+        private readonly JsonSerializerOptions _jsonOptions;
+        public ClassroomClient(string accessToken, HttpClient client, string baseUrl)
         {
-            throw new NotImplementedException();
+            _httpClient = client;
+            _accessToken = accessToken;
+            _baseUrl = baseUrl;
+            _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", _accessToken);
+            _jsonOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = false
+            };
+        }
+        public async Task<TEntity> AddAsync(TEntity entity)
+        {
+            //map to request dto
+            TResponse requestDto = (TResponse)(object)entity;
+            var result = await _httpClient.PostAsJsonAsync(_baseUrl, requestDto, _jsonOptions);
+            result.EnsureSuccessStatusCode();
+            var json = await result.Content.ReadAsStringAsync();
+            var createdDto = JsonSerializer.Deserialize<TResponse>(json, _jsonOptions)!;
+            return (TEntity)(object)createdDto;
+        }
+
+        public async Task DeleteAsync(Guid externalId)
+        {
+            var result = await _httpClient.DeleteAsync($"{_baseUrl}/{externalId}");
+            result.EnsureSuccessStatusCode();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            var res = await _httpClient.GetAsync(_baseUrl);
+            res.EnsureSuccessStatusCode();
+            var json = await res.Content.ReadAsStringAsync();
+            var temp = JsonSerializer.Deserialize<IEnumerable<TResponse>>(json)!;
+            return (IEnumerable<TEntity>)(object)temp;
+        }
+
+        public async Task<TEntity?> GetByIdAsync(Guid externalId)
+        {
+            var res = await _httpClient.GetAsync($"{_baseUrl}/{externalId}");
+            res.EnsureSuccessStatusCode();
+            var json = await res.Content.ReadAsStringAsync();
+            var temp = JsonSerializer.Deserialize<TResponse>(json)!;
+            return (TEntity)(object)temp;
+        }
+
+        public async Task<TEntity> UpdateAsync(TEntity entity, Guid externalId)
+        {
+            var requestDto = (TResponse)(object)entity;
+            var result = await _httpClient.PutAsJsonAsync($"{_baseUrl}/{externalId}", requestDto, _jsonOptions);
+            result.EnsureSuccessStatusCode();
+            return entity;
         }
     }
 }
