d50867aefd93111ca5fb17f37b483ab90c18b3db
illia <illiakrang@gmail.com>
2025-10-12 17:06:55 +0300

    feat: add env variables logic

--- server/LabWorkOrganization.API/Program.cs
+++ server/LabWorkOrganization.API/Program.cs
@@ -1,9 +1,9 @@
+using DotNetEnv;
 using LabWorkOrganization.API;
-
+Env.Load();
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
