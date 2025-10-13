9070dbb0fab4763b12054bdc538748094bc65161
illia <illiakrang@gmail.com>
2025-10-13 13:50:02 +0300

    fix : change parametrs to accept dtos, add validation

--- server/LabWorkOrganization.Application/Services/CourseService.cs
+++ server/LabWorkOrganization.Application/Services/CourseService.cs
@@ -1,10 +1,10 @@
+using LabWorkOrganization.Application.Dtos;
 using LabWorkOrganization.Domain.Entities;
 using LabWorkOrganization.Domain.Intefaces;
 using LabWorkOrganization.Domain.Utilities;
 
 namespace LabWorkOrganization.Application.Services
 {
-    // TODO: all service methods must accept dtos instead of entities
     public class CourseService
     {
         private readonly ICrudRepository<Course> _crudRepository;
@@ -16,17 +16,32 @@ namespace LabWorkOrganization.Application.Services
             _unitOfWork = IUnitOfWork;
             _externalCrudRepository = IExternalCrudRepository;
         }
-        public async Task<Result<Course>> CreateCourse(Course course, bool useExternal)
+        public async Task<Result<Course>> CreateCourse(CourseCreationalDto course, bool useExternal)
         {
             try
             {
+                var errors = Validation.ValidationHelper.Validate(course);
+                if (errors.Count > 0)
+                {
+                    throw new ArgumentException(string.Join("; ", errors));
+                }
+
+                var newCourse = new Course
+                { // NOT ENTERING EXTERNAL ID EXTERNAL API WILL HANDLE IT
+                    Id = Guid.NewGuid(),
+                    OwnerId = Guid.Empty, // NEED TO IMPLEMENT AUTHORIZATION
+                    Name = course.Name,
+                    LessonDuration = course.LessonDuration,
+                    EndOfCourse = course.EndOfCourse
+                };
                 if (useExternal)
                 {
-                    await _externalCrudRepository.AddAsync(course);
+
+                    await _externalCrudRepository.AddAsync(newCourse);
                 }
-                await _crudRepository.AddAsync(course);
+                await _crudRepository.AddAsync(newCourse);
                 await _unitOfWork.SaveChangesAsync();
-                return Result<Course>.Success(course);
+                return Result<Course>.Success(newCourse);
             }
             catch (Exception ex)
             {
@@ -35,10 +50,11 @@ namespace LabWorkOrganization.Application.Services
                 return Result<Course>.Failure($"An error occurred while creating the course: {ex.Message}");
             }
         }
-        public async Task<Result<Course?>> GetCourseById(Guid id, bool external)
+        public async Task<Result<Course?>> GetCourseById(Guid id, bool external = false)
         {
             try
             {
+                if (id.Equals(null)) throw new ArgumentNullException(nameof(id));
                 if (external)
                 {
                     return Result<Course?>.Success(await _externalCrudRepository.GetByIdAsync(id));
@@ -51,7 +67,7 @@ namespace LabWorkOrganization.Application.Services
                 return Result<Course?>.Failure($"An error occurred while creating the course: {ex.Message}");
             }
         }
-        public async Task<Result<IEnumerable<Course>>> GetAllCourses(bool isGetExternal)
+        public async Task<Result<IEnumerable<Course>>> GetAllCourses(bool isGetExternal = false)
         {
             try
             {
@@ -70,10 +86,16 @@ namespace LabWorkOrganization.Application.Services
                 return Result<IEnumerable<Course>>.Failure($"An error occurred while creating the course: {ex.Message}");
             }
         }
-        public async Task<Result<Course>> UpdateCourse(Course course, bool updateExternal)
+        public async Task<Result<Course>> UpdateCourse(Course course, bool updateExternal = false)
         {
             try
             {
+                var errors = Validation.ValidationHelper.Validate(course);
+                if (errors.Count > 0)
+                {
+                    throw new ArgumentException(string.Join("; ", errors));
+                }
+
                 if (updateExternal)
                 {
                     if (course.ExternalId.HasValue)
