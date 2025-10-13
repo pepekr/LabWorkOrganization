4b6e1bd3ba35dcbfa87ce29841ae56f529e2fc5e
illia <illiakrang@gmail.com>
2025-10-13 17:43:22 +0300

    feat: add back hashpassword

--- server/LabWorkOrganization.Domain/Entities/User.cs
+++ server/LabWorkOrganization.Domain/Entities/User.cs
@@ -8,6 +8,7 @@ namespace LabWorkOrganization.Domain.Entities
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
+        public string HashedPassword { get; set; }
         public string SubGoogleId { get; set; }
         public Role Role { get; set; } // e.g., "Student", "Teacher", "Admin"
 
