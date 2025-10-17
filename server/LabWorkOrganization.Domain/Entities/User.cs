4dd0b5cf0ad591f7fb3a97648dc201a0f04746a2
illia <illiakrang@gmail.com>
2025-10-17 18:21:09 +0300

    fix: add nullable to an external id property

--- server/LabWorkOrganization.Domain/Entities/User.cs
+++ server/LabWorkOrganization.Domain/Entities/User.cs
@@ -9,8 +9,8 @@ namespace LabWorkOrganization.Domain.Entities
         public string Name { get; set; }
         public string Email { get; set; }
         public string HashedPassword { get; set; }
-        public string SubGoogleId { get; set; }
-        public Role Role { get; set; } // e.g., "Student", "Teacher", "Admin"
+        public string? SubGoogleId { get; set; }
+        public Role? Role { get; set; } // e.g., "Student", "Teacher", "Admin"
 
         public ICollection<UserTask> UserTasks { get; set; } = new List<UserTask>();
         public ICollection<SubGroup> SubGroups { get; set; } = new List<SubGroup>();
